740f58d83c8a3d50dd808292ace3bea075a12ed7
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-10-19 13:34:36 +0200

    CHECK instead of REQUIRE doctest

--- tests/PlayerTest.cc
+++ tests/PlayerTest.cc
@@ -7,30 +7,30 @@
 using std::vector;
 
 TEST_CASE("Player construction is OK") {
-    REQUIRE_NOTHROW(Player validPlayer("pablorg","pablopass"));
-    REQUIRE_NOTHROW(Player validPlayer("francismp","francispass"));
-    REQUIRE_NOTHROW(Player validPlayer("sergiobachiller","sergiopass"));
+    CHECK_NOTHROW(Player validPlayer("pablorg","pablopass"));
+    CHECK_NOTHROW(Player validPlayer("francismp","francispass"));
+    CHECK_NOTHROW(Player validPlayer("sergiobachiller","sergiopass"));
 }
 
 TEST_CASE("Adding/Getting tile (as Tile object) to player is OK"){
     Player newPlayer("player", "playerpass");
     vector<Tile> playerTiles = {Tile(2,4)};
     newPlayer.addTile(Tile(2,4));
-    REQUIRE(newPlayer.getPlayerTiles() == playerTiles);
+    CHECK(newPlayer.getPlayerTiles() == playerTiles);
     newPlayer.addTile(Tile(3,3));
     playerTiles.push_back(Tile(3,3));
-    REQUIRE(newPlayer.getPlayerTiles() == playerTiles);
+    CHECK(newPlayer.getPlayerTiles() == playerTiles);
     newPlayer.addTile(Tile(1,6));
     playerTiles.push_back(Tile(1,6));
-    REQUIRE(newPlayer.getPlayerTiles() == playerTiles);
+    CHECK(newPlayer.getPlayerTiles() == playerTiles);
 }
 
 TEST_CASE("Getting tile as string from player is OK"){
     Player newPlayer("player", "playerpass");
     newPlayer.addTile(Tile(2,4));
-    REQUIRE(newPlayer.getPlayerTilesString() == "|2|4|");
+    CHECK(newPlayer.getPlayerTilesString() == "|2|4|");
     newPlayer.addTile(Tile(3,3));
-    REQUIRE(newPlayer.getPlayerTilesString() == "|2|4||3|3|");
+    CHECK(newPlayer.getPlayerTilesString() == "|2|4||3|3|");
     newPlayer.addTile(Tile(1,6));
-    REQUIRE(newPlayer.getPlayerTilesString() == "|2|4||3|3||1|6|");
+    CHECK(newPlayer.getPlayerTilesString() == "|2|4||3|3||1|6|");
 }
--- tests/TileTest.cc
+++ tests/TileTest.cc
@@ -4,36 +4,36 @@
 #include "Tile.h"
 
 TEST_CASE("Tile values out of range") {
-    REQUIRE_THROWS(Tile(-1,6));
-    REQUIRE_THROWS(Tile(-1,-1));
-    REQUIRE_THROWS(Tile(-1,7));
-    REQUIRE_THROWS(Tile(1,-1));
-    REQUIRE_THROWS(Tile(7,-1));
-    REQUIRE_THROWS(Tile(8,2));
+    CHECK_THROWS(Tile(-1,6));
+    CHECK_THROWS(Tile(-1,-1));
+    CHECK_THROWS(Tile(-1,7));
+    CHECK_THROWS(Tile(1,-1));
+    CHECK_THROWS(Tile(7,-1));
+    CHECK_THROWS(Tile(8,2));
 }
 
 TEST_CASE("Tile construction is OK") {
-    REQUIRE_NOTHROW(Tile validTile(4,6));
-    REQUIRE_NOTHROW(Tile validTile(2,6));
-    REQUIRE_NOTHROW(Tile validTile(0,0));
+    CHECK_NOTHROW(Tile validTile(4,6));
+    CHECK_NOTHROW(Tile validTile(2,6));
+    CHECK_NOTHROW(Tile validTile(0,0));
 }
 
 TEST_CASE("Tile getters") {
     Tile validTile1(2,3);
     Tile validTile2(0,6);
-    REQUIRE(validTile1.getLeft() == 2);
-    REQUIRE(validTile1.getRight() == 3);
-    REQUIRE(validTile2.getLeft() == 0);
-    REQUIRE(validTile2.getRight() == 6);
+    CHECK(validTile1.getLeft() == 2);
+    CHECK(validTile1.getRight() == 3);
+    CHECK(validTile2.getLeft() == 0);
+    CHECK(validTile2.getRight() == 6);
 }
 
 TEST_CASE("Flip tile") {
     Tile validTile(2,3);
-    REQUIRE(validTile.getLeft() == 2);
-    REQUIRE(validTile.getRight() == 3);
-    REQUIRE_NOTHROW(validTile.flipTile());
-    REQUIRE(validTile.getLeft() == 3);
-    REQUIRE(validTile.getRight() == 2);
+    CHECK(validTile.getLeft() == 2);
+    CHECK(validTile.getRight() == 3);
+    CHECK_NOTHROW(validTile.flipTile());
+    CHECK(validTile.getLeft() == 3);
+    CHECK(validTile.getRight() == 2);
 }
 
 TEST_CASE("== Operator") {
@@ -41,7 +41,7 @@ TEST_CASE("== Operator") {
     Tile EqualTile2(2,3);
     Tile DifferentTile(2,4);
     Tile EqualFlippedTile(3,2);
-    REQUIRE(EqualTile1 == EqualTile2);
-    REQUIRE_FALSE(EqualTile1 == DifferentTile);
-    REQUIRE(EqualTile1 == EqualFlippedTile);
+    CHECK(EqualTile1 == EqualTile2);
+    CHECK_FALSE(EqualTile1 == DifferentTile);
+    CHECK(EqualTile1 == EqualFlippedTile);
 }
