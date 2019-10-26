120bf698120c1a20a7fedf10339171e412674843
rexuswolf <francisjmp99@gmail.com>
2019-10-26 17:54:06 +0200

    Finished v0.5

--- src/Server.cc
+++ src/Server.cc
@@ -8,56 +8,182 @@
 #include <string.h>
 #include <iostream>
 
-Server::Server(int serverPort, int serverCapacity) {
-    this->serverCapacity_ = serverCapacity;
-    this->serverSocketDescriptor_ = socket (AF_INET, SOCK_STREAM, 0);
-    if (this->serverSocketDescriptor_ == -1)
-	{
-		perror("Error opening server socket\n");
-    		exit (1);	
-	}
-    this->serverSocketData_.sin_family = AF_INET;
-    this->serverSocketData_.sin_port = htons(serverPort);
-    this->serverSocketData_.sin_addr.s_addr = INADDR_ANY;
+#define MAX_CLIENTS 30
 
-    if (bind (this->getServerSocketDescriptor_(), this->getFormattedServerSocketAddress_(), this->getServerSocketDataSize_() == -1))
-	{
-		perror("Error with bind operation");
-		exit(1);
-	}
+Server::Server(int serverPort) {
+    openServerSocket_();
+    fillServerSocketDataStructure_(serverPort);
+    assingServerAddressToServerSocket_();
+    markServerSocketAsPassiveSocket_();
+    numberOfClients_ = 0;
+}
 
-    if(listen(this->serverSocketDescriptor_, this->serverCapacity_) == -1) {
-        perror("Error with listen operation");
+void Server::openServerSocket_() {
+    serverSocketDescriptor_ = socket (AF_INET, SOCK_STREAM, 0);
+    if(serverSocketDescriptor_ == -1) {
+        cerr << "Could not open server socket" << endl;
         exit(1);
     }
 }
 
-void Server::clearFDSet() {
-    FD_ZERO(&this->ReadSet_);
-    FD_SET(this->serverSocketDescriptor_, &this->ReadSet_);
+void Server::fillServerSocketDataStructure_(int serverPortNumber) {
+    serverSocketData_.sin_family = AF_INET;
+    serverSocketData_.sin_addr.s_addr = INADDR_ANY;
+    serverSocketData_.sin_port = htons(serverPortNumber);
+}
 
-    for (auto clientSocketDescriptor = this->getClients_().begin(); clientSocketDescriptor != this->getClients_().end(); ++clientSocketDescriptor) {
-        FD_SET(*clientSocketDescriptor, &this->ReadSet_);
+void Server::assingServerAddressToServerSocket_() {
+    int bindResult = bind(getServerSocketDescriptor_(), getFormattedServerSocketData_(), getSizeOfServerSocketData_());
+    if (bindResult == -1) {
+		cerr << "Error with bind operation" << endl;
+		exit(1);
+	}
+}
+
+void Server::markServerSocketAsPassiveSocket_() {
+    int listenResult = listen(serverSocketDescriptor_, MAX_CLIENTS);
+    if(listenResult == -1) {
+        cerr << "Error with listen operation" << endl;
+        exit(1);
     }
 }
 
-void Server::searchForMatch() {
-    char buffer[128];
-    int connectionSocketDescriptor;
-    
+void Server::setFileDescriptorStructures_() {
+    FD_ZERO(&readerFileDescriptor_);
+    FD_ZERO(&auxiliarFileDescriptor_);
+    FD_SET(0, &readerFileDescriptor_);
+    FD_SET(serverSocketDescriptor_, &readerFileDescriptor_);
+}
 
-    while(1){
-        this->clearFDSet();
+void Server::handleNewClient_() {
 
-        select(FD_SETSIZE, &this->ReadSet_, NULL, NULL, NULL);
-        for (auto clientSocketDescriptor = this->getClients_().begin(); clientSocketDescriptor != this->getClients_().end(); ++clientSocketDescriptor) {
-            if(FD_ISSET(*clientSocketDescriptor, &this->ReadSet_)) {
-                if( *clientSocketDescriptor == this->getServerSocketDescriptor_()){
-                    if((connectionSocketDescriptor = accept(this->getServerSocketDescriptor_(), this->getFormattedHostInfo_(), this->getSizeOfHost_())) == -1){
-                        perror("Error accepting requests");
-                    }
-            }
-        }   
+    newClientSocketDescriptor_ = accept(getServerSocketDescriptor_(), getFormattedClientSocketData_(), getSizeOfClientSocketData_());
+
+    if(getNewClientSocketDescriptor_() == -1) {
+        cerr << "Error accepting requests" << endl;
+        exit(1);
     }
 
+    else if(getNumberOfClients_() < MAX_CLIENTS) {
+        addClientToServer_();
+    }
+    
+    else {
+        sendTooManyClientsMessageToNewClient_();
+    }
+}
+
+void Server::addClientToServer_() {
+    getClients_().push_back(getNewClientSocketDescriptor_());
+    numberOfClients_++;
+    FD_SET(getNewClientSocketDescriptor_(), getReaderFileDescriptor_());
+    send(getNewClientSocketDescriptor_(), "Welcome to Server\n", strlen("Welcome to Server\n"), 0);
+}
+
+void Server::sendTooManyClientsMessageToNewClient_() {
+    FD_SET(getNewClientSocketDescriptor_(), getReaderFileDescriptor_());
+    send(getNewClientSocketDescriptor_(), "Too many clients connected\n", strlen("Too many clients connected\n"), 0);
+}
+
+void Server::searchForMatch() {
+
+    int socketID;
+    int receivedMessages;
+    
+    while(1){
+
+        auxiliarFileDescriptor_ = readerFileDescriptor_;
+
+        int exit = select(FD_SETSIZE, &this->readerFileDescriptor_, NULL, NULL, NULL);
+
+        if ( exit > 0 ){
+            for(socketID = 0; socketID < FD_SETSIZE; socketID++){
+                if(FD_ISSET(socketID, &auxiliarFileDescriptor_)){
+                    if(socketID == serverSocketDescriptor_){
+                        this->handleNewClient_();
+                    }
+                    else if(socketID == 0){
+                        this->serverMessageHandler();
+                    }
+                    else{
+                        memset(messageBuffer_, 0, sizeof(messageBuffer_));
+                            
+                        receivedMessages = recv(socketID,messageBuffer_,sizeof(messageBuffer_),0);
+                        if (receivedMessages > 0){
+                            clientMessageHandler(socketID);
+                        }
+                        if(receivedMessages == 0){
+                            printf("The client %d, left the game\n", socketID);
+                            exitClient(socketID, getReaderFileDescriptor_(), this->getNumberOfClients_(), this->getClients_());
+                        }
+                    }
+                }
+            }
+        }
+    }
+    close(serverSocketDescriptor_);
+    exit(-1);
+}
+
+void Server::serverMessageHandler(){
+    memset(messageBuffer_, 0, sizeof(messageBuffer_));
+    fgets(messageBuffer_, sizeof(messageBuffer_),stdin);
+    if (strcmp(messageBuffer_,"EXIT\n") == 0){
+        this->closeServer_();
+    }
+    // Add server to clients messages
+
+    
+}
+
+void Server::closeServer_(){
+    for (auto clientSocketDescriptor = Clients_.begin(); clientSocketDescriptor != Clients_.end(); ++clientSocketDescriptor){
+        send(*clientSocketDescriptor, "Server is closing\n", strlen("Server is closing\n"),0);
+        close(*clientSocketDescriptor);
+        FD_CLR(*clientSocketDescriptor, getReaderFileDescriptor_());
+    }
+    close(serverSocketDescriptor_);
+    exit(-1);
+}
+
+void Server::clientMessageHandler(int socketID){
+    if(strcmp(messageBuffer_,"EXIT\n") == 0){                               
+        cout << "The client %d, left the game" << endl;
+        exitClient(socketID, getReaderFileDescriptor_(), this->getNumberOfClients_(), this->getClients_());
+    }
+    else{
+        sprintf(messageIdentifier_, "%d: %s", socketID, messageBuffer_);
+        memset(messageBuffer_, 0, sizeof(messageBuffer_));
+        strcpy(messageBuffer_,messageIdentifier_);
+        
+        for (auto clientSocketDescriptor = Clients_.begin(); clientSocketDescriptor != Clients_.end(); ++clientSocketDescriptor){
+            if(*clientSocketDescriptor != socketID){
+                send(*clientSocketDescriptor,messageBuffer_,strlen(messageBuffer_),0);        
+            }
+        }
+    }
+}
+
+void Server::exitClient(int socketID, fd_set * readerFileDescriptor_, int numberOfClients , vector <int> Clients){
+    close(socketID);
+    FD_CLR(socketID,readerFileDescriptor_);
+    
+    for (auto clientSocketDescriptor = Clients_.begin(); clientSocketDescriptor != Clients_.end(); ++clientSocketDescriptor) {
+        if(*clientSocketDescriptor != socketID){
+            break;     
+        }
+    }
+    for (auto clientSocketDescriptor = Clients_.begin(); clientSocketDescriptor != Clients_.end(); ++clientSocketDescriptor) {
+        clientSocketDescriptor = clientSocketDescriptor++;
+    }
+    this->numberOfClients_--;
+    
+    memset(messageBuffer_, 0, sizeof(messageBuffer_));
+    sprintf(messageBuffer_, "Desconexión del cliente: %d\n", socketID)  ;
+    
+    for (auto clientSocketDescriptor = Clients_.begin(); clientSocketDescriptor != Clients_.end(); ++clientSocketDescriptor) {
+        if(*clientSocketDescriptor != socketID){
+            send(*clientSocketDescriptor, messageBuffer_, strlen(messageBuffer_), 0);     
+        }
+    }
 }
--- src/include/Server.h
+++ src/include/Server.h
@@ -1,40 +1,84 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <unistd.h>
-// socket function and parameters
-#include <sys/socket.h>
 #include <sys/types.h>
+// socket() and parameters
+#include <sys/socket.h>
+// sockaddr_in structure
 #include <netinet/in.h>
-#include <netdb.h>
+// htons(), inet_addr()
+#include <arpa/inet.h>
+// close()
+#include <unistd.h>
+// bzero()
+#include <cstring>
+
+// String
+#include <string>
+using std::string;
+
 // STL Data Types
 #include <vector>
-#include <string>
 using std::vector;
 using std::size_t;
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+using std::cerr;
 
 class Server {
 
     private:
         int serverSocketDescriptor_;
-        int serverCapacity_;
-        struct sockaddr_in serverSocketData_;
-        struct sockaddr_in hostInfo_;
-        socklen_t hostSize_;
-        vector<int> Clients_;
-        fd_set ReadSet_;
-        inline const struct sockaddr * getFormattedServerSocketAddress_() {return (struct sockaddr *) &this->serverSocketData_;}
         inline int getServerSocketDescriptor_() {return serverSocketDescriptor_;}
-        inline socklen_t getServerSocketDataSize_() {return sizeof(&this->serverSocketData_);}
-        inline vector<int> getClients_(){return Clients_;}
-        inline struct sockaddr * getFormattedHostInfo_(){return (struct sockaddr *) &this->hostInfo_;}
-        inline socklen_t * getSizeOfHost_(){return (socklen_t *) sizeof(this->hostInfo_);}
+        
+        int newClientSocketDescriptor_;
+        inline int getNewClientSocketDescriptor_() {return newClientSocketDescriptor_;}
+        
+        struct sockaddr_in serverSocketData_;
+        inline struct sockaddr * getFormattedServerSocketData_() {return (struct sockaddr *) &this->serverSocketData_;}
+        inline socklen_t getSizeOfServerSocketData_() {return sizeof(serverSocketData_);}
+        
+        struct sockaddr_in clientSocketData_;
+        inline struct sockaddr * getFormattedClientSocketData_() {return (struct sockaddr *) &clientSocketData_;}
+        inline socklen_t * getSizeOfClientSocketData_(){return (socklen_t *) sizeof(this->clientSocketData_);}
+                
+        int numberOfClients_;
+        inline int getNumberOfClients_() {return numberOfClients_;}
+        
+        char messageBuffer_[128];
+        char messageIdentifier_[256];
+        
+        vector <int> Clients_;
+        inline vector <int> getClients_() {return Clients_;}
+        
+        fd_set readerFileDescriptor_;
+        inline fd_set * getReaderFileDescriptor_() {return &readerFileDescriptor_;}
+
+        fd_set auxiliarFileDescriptor_;
+        inline fd_set * getAuxiliarFileDescriptor_() {return &auxiliarFileDescriptor_;}
+        
+
+        void openServerSocket_();
+        void fillServerSocketDataStructure_(int serverPortNumber);
+        void assingServerAddressToServerSocket_();
+        void markServerSocketAsPassiveSocket_();
+        void setFileDescriptorStructures_();
+        
+        void handleNewClient_();
+        void addClientToServer_();
+        void sendTooManyClientsMessageToNewClient_();
 
     public:
-        Server(int serverPort, int serverCapacity);
+        Server(int serverPort);
         void clearFDSet();
-        void handleMessage(int clientSocketDescriptor, const char* message);
+        void serverMessageHandler();
+        void clientMessageHandler(int socketID);
         void searchForMatch();
+        void exitClient(int socketID, fd_set * readerFileDescriptor_, int numberOfClients , vector <int> Clients);
+        void closeServer_();
 };   
 
 
